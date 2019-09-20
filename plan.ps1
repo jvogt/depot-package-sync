bd806d9d97f2ea9153542369686a3aad7c8c4e5a
Jeff Vogt <jvogt@chef.io>
2019-09-20 13:27:33 -0700

    bugfixes and cleanup

--- plan.ps1
+++ plan.ps1
@@ -4,12 +4,13 @@ $pkg_version="0.1.0"
 $pkg_description="Helps sync from public to private depot"
 $pkg_maintainer="Jeff Vogt <jdvogt@gmail.com>"
 $pkg_license=("Apache-2.0")
-$pkg_bin_dirs=@(bin)
+$pkg_bin_dirs=@('bin')
 
 function invoke-build { }
 
 function invoke-install {
-  mkdir $pkg_prefix\bin
-  copy-item $PLAN_CONTEXT\depot-package-sync.ps1 $pkg_prefix\bin -force
-  copy-item $PLAN_CONTEXT\workshop-packages $PLAN_CONTEXT\README.md $pkg_prefix\ -force
+  mkdir $pkg_prefix\bin -force
+  copy-item $PLAN_CONTEXT\depot-package-sync.psm1 $pkg_prefix\bin -force
+  copy-item $PLAN_CONTEXT\workshop-packages $pkg_prefix\ -force
+  copy-item $PLAN_CONTEXT\README.md $pkg_prefix\ -force
 }
